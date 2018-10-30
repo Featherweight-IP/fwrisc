d52683da65751fab80ffa7c29e8a74f88b965552
Matthew Ballance <matt.ballance@gmail.com>
2018-10-30 09:12:53 -0700

    add tracer bfm

---
+++ ve/fwrisc_tracer_bfm/gtvl/fwrisc_tracer.cpp
@@ -0,0 +1,18 @@
+/*
+ * fwrisc_tracer.cpp
+ *
+ *  Created on: Oct 29, 2018
+ *      Author: ballance
+ */
+
+#include "fwrisc_tracer.h"
+
+fwrisc_tracer::fwrisc_tracer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+fwrisc_tracer::~fwrisc_tracer() {
+	// TODO Auto-generated destructor stub
+}
+
