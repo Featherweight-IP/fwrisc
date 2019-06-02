509da5526906cb97ed4e0184965bf88e41d52679
Matthew Ballance <matt.ballance@gmail.com>
2019-06-02 12:15:45 -0400

    beginnings of pyHPI-based environment

---
+++ ve/fwrisc/sim/scripts/vlog_vl-pyhpi.f
@@ -0,0 +1,10 @@
+
+-f ${MEMORY_PRIMITIVES}/rtl/sim/sim.f
+
+-f ${FWRISC}/rtl/fwrisc.f
+${FWRISC}/ve/fwrisc_tracer_bfm/fwrisc_tracer_bfm.sv
+-F ${FWRISC}/ve/fwrisc/tb/tb.F
+
+// -f ${FWRISC}/ve/fwrisc/sim/scripts/vlog_hdl.f
+// -f ${FWRISC}/ve/fwrisc/sim/scripts/vlog_hvl_ms.f
+
---
+++ ve/fwrisc/tests/py-hpi/fwrisc_tests/base.py
@@ -0,0 +1,5 @@
+'''
+Created on Jun 1, 2019
+
+@author: ballance
+'''
